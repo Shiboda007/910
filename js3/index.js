9fbe69ff61a60bb033dacc1b9cc54572278281a8
Shiboda007 <119075203+Shiboda007@users.noreply.github.com>
2022-11-30 21:20:18 +0500

    Update index.js

--- js3/index.js
+++ js3/index.js
@@ -1,22 +1,22 @@
 
-let name = prompt("what is your name?").toUpperCase().trim()
-let age = prompt("how old are you?")
-let money = prompt("how much money do you have?")
-let people = prompt("How many people want to enter the club?")
-if(name[0] ==="A" && age >20 && age <=40 && money >100 && people < 10){
-alert('welcome');
-}
-else{
-    alert("go home");
-}
+// let name = prompt("what is your name?").toUpperCase().trim()
+// let age = prompt("how old are you?")
+// let money = prompt("how much money do you have?")
+// let people = prompt("How many people want to enter the club?")
+// if(name[0] ==="A" && age >20 && age <=40 && money >100 && people < 10){
+// alert('welcome');
+// }
+// else{
+//     alert("go home");
+// }
 
-let num = [1, 2, 3, 4, 5 ] 
-let all = +prompt (' ')
-if (num.length + all  <= 10) {
-     alert('Welcome')
- } else { 
-     alert(' Go home')
-}
+// let num = [1, 2, 3, 4, 5 ] 
+// let all = +prompt (' ')
+// if (num.length + all  <= 10) {
+//      alert('Welcome')
+//  } else { 
+//      alert(' Go home')
+// }
 // let activeName = 'Alex'
 // let activeAccount = 7777
 // let activeMoney = 10000
@@ -42,3 +42,23 @@ if (num.length + all  <= 10) {
 // if(activeMoney - end){
 //     alert(`У вас было ${activeMoney} У вас осталось ${activeMoney - end}`)
 //     }
+
+
+let numbers = [5,2,1,4,11,8,7,25, 37, 2,6,1];
+
+
+// let isOdd = (num) => num % 2 !== 0;
+
+
+// let odd = numbers.filter(isOdd);
+
+// console.log(odd);
+
+
+let even = (num) => num % 2 === 0;
+
+
+let isEven = numbers.filter(even);
+
+console.log(isEven);
+
