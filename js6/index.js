298bcbd903b9576d5326ed4df374317ddc5008d8
Shiboda007 <119075203+Shiboda007@users.noreply.github.com>
2022-12-10 09:23:34 +0500

    Add files via upload

---
+++ js6/index.js
@@ -0,0 +1,23 @@
+let assigned = Object.assign({}, )
+let key = Object.keys()
+let value = Object.values()
+let arrAssigned = [].concat( key,value )
+
+let types = {
+    string: [],
+    number: [],
+    object: [],
+    boolean: []
+}
+arrAssigned.filter(item => {
+    if (typeof(item) === 'string') {
+        types.string.push(item)
+    } else if (typeof(item) === 'number') {
+        types.number.push(item)
+    } else if (typeof(item) === 'object') {
+        types.object.push(item)
+    } else {
+        types.boolean.push(item)
+    }
+})
+console.log(types);
